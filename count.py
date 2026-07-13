c075db78256638f75f35b63fad376cd030d9a013
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-07-14 00:44:21 +0530

    added basic count commands

---
+++ count.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+
+
+df=pd.read_csv("data.csv")
+print(df.count()) ##This will print the count of non-null values in each column of the DataFrame
+
+print(df['Duration']).value_counts()  ##This will print the count of unique values in the "Type 1" column
+
+print(df['Duration'].unique())  ##This will print the unique values in the "Type 1" column
+
+print(df['Duration'].nunique())  ##This will print the number of unique values in the "Type 1" column
