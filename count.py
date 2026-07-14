d8d2d68771f4f930e88e8c4af180ee107327e2cf
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-07-14 23:47:29 +0530

    added the concept of skewness

--- count.py
+++ count.py
@@ -1,11 +1,20 @@
-import pandas as pd 
+import pandas as pd
 
+df = pd.read_csv("data.csv")
+print(
+    df.count()
+)  ##This will print the count of non-null values in each column of the DataFrame
 
-df=pd.read_csv("data.csv")
-print(df.count()) ##This will print the count of non-null values in each column of the DataFrame
+print(
+    (df["Calories"]).value_counts()
+)  ##This will print the count of unique values in the "Type 1" column
 
-print(df['Duration']).value_counts()  ##This will print the count of unique values in the "Type 1" column
+print(
+    df["Duration"].unique()
+)  ##This will print the unique values in the "Type 1" column
 
-print(df['Duration'].unique())  ##This will print the unique values in the "Type 1" column
+print(
+    df["Duration"].nunique()
+)  ##This will print the number of unique values in the "Type 1" column
 
-print(df['Duration'].nunique())  ##This will print the number of unique values in the "Type 1" column
+print(df["Duration"].skew())
