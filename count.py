b7126b4f13c6910512a0f0e93e7227b46d06b6dd
Siddhesh2008 <siddheshbala2008@gmail.com>
2026-07-15 00:21:21 +0530

    added kurt and commented

--- count.py
+++ count.py
@@ -6,15 +6,17 @@ print(
 )  ##This will print the count of non-null values in each column of the DataFrame
 
 print(
-    (df["Calories"]).value_counts()
-)  ##This will print the count of unique values in the "Type 1" column
+    (df["Duration"]).value_counts()
+)  ##This will print the count of unique values in the "Duration" column
 
 print(
     df["Duration"].unique()
-)  ##This will print the unique values in the "Type 1" column
+)  ##This will print the unique values in the "Duration" column
 
 print(
     df["Duration"].nunique()
-)  ##This will print the number of unique values in the "Type 1" column
+)  ##This will print the number of unique values in the "Duration" column
 
-print(df["Duration"].skew())
+print(df["Duration"].skew())  ##This will print the skewness of the "Duration" column
+
+print(df["Duration"].kurt())  ##This will print the kurtosis of the "Duration" column
